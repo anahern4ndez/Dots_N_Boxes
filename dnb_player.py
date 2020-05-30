953194bb3e27e63a86b5efdfecde07074dd057f3
Ana Hernandez <her17138@uvg.edu.gt>
2020-05-29 22:45:14 -0600

    random player

--- dnb_player.py
+++ dnb_player.py
@@ -1,12 +1,12 @@
 
-import socket
-
 HOST = '3.12.129.126'  # Standard loopback interface address
 # HOST = 'localhost'  # Standard loopback interface address
 PORT = 4000        # Port to listen on (non-privileged ports are > 1023)
 USERNAME = 'ana lucia hernandez'
-TOURNAMENT_ID = 1000
+TOURNAMENT_ID = 1
 
+CURRENT_GAME_ID = -1
+PLAYER_TURN_ID = -1
 # with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
 #     s.connect((HOST, PORT))
 #     s.sendall(b'Hello, world')
@@ -14,6 +14,7 @@ TOURNAMENT_ID = 1000
 #     print(data)
 
 import socketio
+import random
 
 sio = socketio.Client()
 
@@ -26,14 +27,32 @@ def connect():
         'user_role': 'player'
     })
 
-@sio.event
-def my_message(data):
-    print('message received with ', data)
-    sio.emit('signin', {
-        'user_name': USERNAME,
-        'tournament_id': TOURNAMENT_ID,
-        'user_role': 'player'
+@sio.on('ready')
+def play(data):
+    print(data)
+    CURRENT_GAME_ID = data['game_id']
+    PLAYER_TURN_ID = data['player_turn_id']
+    narray = random.randint(0,1)
+    line = random.randint(0,29)
+    print("Movement made:" + str(narray) + ", " + str(line))
+    sio.emit('play', {
+          'tournament_id': TOURNAMENT_ID,
+          'player_turn_id': PLAYER_TURN_ID,
+          'game_id': CURRENT_GAME_ID,
+          'movement': [narray, line]
     })
+
+@sio.on('finish')
+def finish(data):
+    print("\nTerminó el juego.")
+    if (data['player_turn_id'] != data['winner_turn_id']): print('\t->>>>>> Perdiste :(')
+    else: print('\t->>>>>> Ganaste :)\n')
+    sio.emit('player_ready', {
+        'tournament_id': TOURNAMENT_ID,
+        'player_turn_id': data['player_turn_id'],
+        'game_id': data['game_id']
+    })
+
 
 @sio.on('ok_signin')
 def ok_signin():
